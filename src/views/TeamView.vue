3f6c51b4a98b5e437c159aa94961c6b7f99ad0fe
petrero <petrero@gmail.com>
2024-01-13 13:56:05 +0200

    26.8 Okay. So the next step is to add a bit of logic to determine if we have any spots remaining on our team.
     We might initially say here's our button disable the button if the team members or the number of the team members equals the team spots.
    
     If that's the case, then it should be disabled. And in this case if we come back sure enough it is but yeah if we were to test this.
    
     Let's assume you have seven spots. Well now we should have two more. So come back and refresh and it's no longer disabled. Okay, next. Let's make zero spots left dynamic.
    
     So we could say right here. What would we do team dot spots minus team dot members dot length again things like this can be great candidates for computed Properties or Getters if we were to use store and Pina terminology and we'll do that later.
    
     So come back and refresh and sure enough. This is now Dynamic the label is dynamic.

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -9,9 +9,9 @@
     <div>
       <button
         class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-        :disabled="true"
+        :disabled="team.members.length === team.spots"
       >
-        Add Member (0 Spots Left)
+        Add Member ({{ team.spots - team.members.length }} Spots Left)
       </button>
     </div>
     <div>
