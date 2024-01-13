6f85ff1a022175aeb2e13bf55aab7613cb3b6bd4
petrero <petrero@gmail.com>
2024-01-13 18:03:27 +0200

    27.1 Build and Seed a Team Store  Okay. So check this out in this episode. We're going to switch over to using a dedicated Pina store for our team.
     And when we do that, we're going to obliterate that entire prop drilling problem that we've been running into over the last few episodes. So check this out. Let's get going. Now the first step of course is to create our store.
    
     So let's add within the stores directory a file called teamstore.js and like we've learned we need to define a new store. So we will import that from Pena and I'll shorten this.
    
     And the name will be team and here is our object. And again, this is where we would declare the state and the actions and to the getters.
    
     So at the moment, I'm going to keep doing State like this but in a little bit we're gonna switch over to using an arrow function. But yeah, you can imagine we need the team's name. We need the team spots. I think that's what we call it. And then we need an array of the team members. Okay. So let's name that and again using convention we would call it used team store and I will export that.

---
+++ src/stores/TeamStores.js
@@ -0,0 +1,11 @@
+import {defineStore} from "pinia";
+
+export let useTeamStore = defineStore('team', {
+  state() {
+    return {
+      name: '',
+      spots: 0,
+      members: []
+    }
+  }
+})
