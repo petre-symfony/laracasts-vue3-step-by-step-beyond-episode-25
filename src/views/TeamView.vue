fd18dddc8ba93b30adbb66767add7f20c2927273
petrero <petrero@gmail.com>
2024-01-13 12:49:34 +0200

    26.3 Okay. So now this seems to be dynamic which means if I switch back to our team view I can now at the top import our new component team member from And what is it components teams and team member Dot View?
     Okay. So now if we scroll down I can grab any one of these table rows comment it out. And if I switch back we can see now we only have four. Okay, let's replace it with our new team member component where we give the name James still.
    
     The email James at doe.com and the status which is active.
    
     Okay. So now if I switch back, there we go, but now we're using a custom component there. So what I'll do now is get rid of all of this and I'm going to repeat this for Sarah so I'd get rid of that another one for Steven another one for Jen and another one for Joe and I won't make you watch this whole process. There we go. So we now have five instances of this team member component and everything looks just the way it did before. Okay, great and yet still I don't know how realistic this is in real life. This data would probably come from a database or an API call.

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+  import TeamMember from "@/components/Teams/TeamMember.vue";
+</script>
 
 <template>
   <header class="flex justify-between">
@@ -31,60 +33,11 @@
         <th class="text-left px-6 py-2">Status</th>
       </thead>
       <tbody>
-        <tr class="bg-gray-100 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img src="https://i.pravatar.cc/50?u=john@doe.com" alt="" class="rounded-full" />
-            John Doe
-          </td>
-          <td class="text-gray-500 px-6 py-4">john@doe.com</td>
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-100 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img src="https://i.pravatar.cc/50?u=sarah@doe.com" alt="" class="rounded-full" />
-            Sarah Doe
-          </td>
-          <td class="text-gray-500 px-6 py-4">sarah@doe.com</td>
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-100 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img src="https://i.pravatar.cc/50?u=steven@doe.com" alt="" class="rounded-full" />
-            Steven Doe
-          </td>
-          <td class="text-gray-500 px-6 py-4">steven@doe.com</td>
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-100 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img src="https://i.pravatar.cc/50?u=jen@doe.com" alt="" class="rounded-full" />
-            Jen Doe
-          </td>
-          <td class="text-gray-500 px-6 py-4">jen@doe.com</td>
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-
-        <tr class="bg-gray-100 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img src="https://i.pravatar.cc/50?u=joe@doe.com" alt="" class="rounded-full" />
-            Joe Doe
-          </td>
-          <td class="text-gray-500 px-6 py-4">joe@doe.com</td>
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
+        <TeamMember name="James Doe" email="james@doe.com" status="Active"></TeamMember>
+        <TeamMember name="John Doe" email="john@doe.com" status="Active"></TeamMember>
+        <TeamMember name="Sarah Doe" email="sarah@doe.com" status="Active"></TeamMember>
+        <TeamMember name="Steven Doe" email="steven@doe.com" status="Active"></TeamMember>
+        <TeamMember name="Jen Doe" email="jen@doe.com" status="Active"></TeamMember>
       </tbody>
     </table>
     <p class="text-right text-gray-600 italic">
