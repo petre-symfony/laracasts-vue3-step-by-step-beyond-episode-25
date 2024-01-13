2d7e877e46fa3d43fc371d0f1f34c21a919c8d4a
petrero <petrero@gmail.com>
2024-01-13 14:47:11 +0200

    26.11 Okay next maybe this full table could be its own component and maybe we'd call it something like a team. Let's see. How about team?
     Members team members list whatever you want.
    
     team members stop you and I'll paste all of that in and once again, I'm going to expect that you give us the team itself.
    
     Or the members now again a quick note whether the template is at the top or the bottom. It's just a preference thing. When I first got started. It was very common for the template to go on the top but these days I often see the script portion go at the top with the template below it and then the style tag below that it doesn't really matter. Whatever you want. Okay, so let's switch back to teamview and now here we'll have our team members where we will once again pass through the team.
    
     And I'll add the extension there.
    
     Okay, so now I can remove this support because team member is being referenced from this component here. So I will add that as well.

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import TeamMember from "@/components/Teams/TeamMember.vue";
+  import TeamMembers from "@/components/Teams/TeamMembers.vue";
   import TeamHeader from "@/components/Teams/TeamHeader.vue";
   import team from "@/team.json";
 </script>
@@ -8,16 +8,7 @@
   <TeamHeader :team="team" />
 
   <div class="place-self-center flex flex-col gap-y-3">
-    <table class="table-fixed border-spacing-2 border-separate">
-      <thead>
-        <th class="text-left px-6 py-2">Name</th>
-        <th class="text-left px-6 py-2">Email</th>
-        <th class="text-left px-6 py-2">Status</th>
-      </thead>
-      <tbody>
-        <TeamMember v-for="member in team.members" :name="member.name" :email="member.email" :status="member.status"></TeamMember>
-      </tbody>
-    </table>
+    <TeamMembers :team="team" />
     <p class="text-right text-gray-600 italic" v-show="team.members.length === team.spots">
       There are no remaining team spots. Upgrade to add more.
     </p>
