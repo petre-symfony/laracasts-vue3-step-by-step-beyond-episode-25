cdff1f37bde59478e7e452c7c6eafbe906648575
petrero <petrero@gmail.com>
2024-01-13 19:26:13 +0200

    27.12 So the final thing I'm going to show you right here is how we declare the state so often if you're reading documentation or tutorials, you might instead see them declare it with a arrow function.
     Now the reason why they do this is it provides some better typescript support in our case. We're not using typescript so it doesn't really matter. But again, it's probably best to get in the habit of this so you may see people structure it like this or if you want to do it in line.
    
     like this Well, just remember a single line Arrow function that needs to return an object. You must wrap that within parentheses and that should do the trick. So again, this is a very common approach for declaring your state and to tell you the truth. It's almost always the approach that I take so I'd recommend it. All right, that'll do it for this video. Really good work. I know this is tough to understand to figure out how you do it and why you would do it and when you would reach for these but trust me you're making good progress.

--- src/stores/TeamStores.js
+++ src/stores/TeamStores.js
@@ -1,13 +1,11 @@
 import {defineStore} from "pinia";
 
 export let useTeamStore = defineStore('team', {
-  state() {
-    return {
-      name: '',
-      spots: 0,
-      members: []
-    }
-  },
+  state: () => ({
+    name: '',
+    spots: 0,
+    members: []
+  }),
   actions: {
     async fill() {
       let r = await import('@/team.json');
