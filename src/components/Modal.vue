d7d5a0a940da35042eb224c5a61099f1db8b552a
petrero <petrero@gmail.com>
2024-01-14 12:59:53 +0200

    29.6 Come back and it should still work and it does and yeah when I say I'm being explicit.
     Here's what I mean. Think about this right here Leaf from class. So that would be the starting class when I begin to leave or hide the element. But right now the opacity is a hundred percent so notice I don't technically need this I could remove this and it would still work go to try show hide show hide.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -10,7 +10,6 @@
       enter-to-class="opacity-100"
       enter-active-class="transition duration-300"
       leave-active-class="transition duration-200"
-      leave-from-class="opacity-100"
       leave-to-class="opacity-0"
   >
     <div v-if="show" class="modal-mask">
