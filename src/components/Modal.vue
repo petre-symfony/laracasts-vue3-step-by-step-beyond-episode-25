63c23ebf394758a314213b1754f321b21bb71e2f
petrero <petrero@gmail.com>
2024-01-14 12:55:41 +0200

    29.5 Okay. Now there's an additional class called enteractive. So as you can imagine this represents CSS classes that will be added to the element for the full length of the transition not at the beginning not at the end, but for the full length, so often this is a good place to put your transitions things like this.
     Now I could remove it like so and that just makes it a little easier to understand what exactly is going on here. Okay. I'm changing the opacity to 100% And the same would be true for leave active class transition duration 200 and now I can remove a little bit of that duplication.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -6,10 +6,12 @@
 
 <template>
   <transition
-      enter-from-class="transition duration-300 opacity-0"
-      enter-to-class="transition duration-300 opacity-100"
-      leave-from-class="transition duration-200 opacity-100"
-      leave-to-class="transition duration-200 opacity-0"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      enter-active-class="transition duration-300"
+      leave-active-class="transition duration-200"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
   >
     <div v-if="show" class="modal-mask">
       <div class="modal-container">
