54ca36fd726a75b0b37650d0fe30db655636d5fd
petrero <petrero@gmail.com>
2024-01-14 13:04:49 +0200

    29.8 when I end think about it, the display is always a hundred percent when we show it. So yet again, this is optional give it a try and it still works but I include it to say it two times now. I include It For My Future Self.

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,6 +7,7 @@
 <template>
   <transition
       enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
       enter-active-class="transition duration-300"
       leave-active-class="transition duration-200"
       leave-from-class="opacity-100"
