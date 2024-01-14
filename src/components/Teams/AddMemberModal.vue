1355724af47d30ee419914c56f3e34ee8db577a3
petrero <petrero@gmail.com>
2024-01-14 15:13:53 +0200

    30.4 cool So now if I switch over to this new component, let's have a look here. We no longer emit an event.
     Now it looks like we need to access information about the number of spots remaining. You know, what you could pass that in as a prop or you could reference the team store that we created. Either one would be fine in my mind.
    
     Let's use the store since we're still learning how to work with that use teamstore. And then let team equals use team store and that should do it. So now the only remaining step is to handle this button. When you click on me show modal becomes true.
    
     All right, and I think we're all set. So admittedly whoops admittedly. We did a bit of refactoring there the cross your fingers and let's see how it turned out.
    
     We give it a try and there we go. It works exactly the way it did before but now the key thing to understand here and what's most important is how the button is now separate from the HTML that is being teleported to the bottom of the document. So now we get the benefit of keeping them together, but we don't pay any penalty for doing so any styling or positioning penalty for doing so So now one last little hiccup that you might run into on your own in terms of where you teleport to just keep in mind that it needs to be something that currently exists in the Dom.

--- src/components/Teams/AddMemberModal.vue
+++ src/components/Teams/AddMemberModal.vue
@@ -1,14 +1,17 @@
 <script setup>
   import Modal from "@/components/Modal.vue";
   import { ref } from "vue";
+  import { useTeamStore } from "@/stores/TeamStores.js";
 
   let showModal = ref(false);
+  let team = useTeamStore();
 </script>
 
 <template>
   <button
       class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
       :disabled="!team.spotsRemaining"
+      @click="showModal = true"
   >
     Add Member ({{ team.spotsRemaining }} Spots Left)
   </button>
